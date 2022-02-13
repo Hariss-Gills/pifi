1cd7f53c046fb53bd553a4a7d8381cdda1da07e5
Hariss Ali Gills <harissgills@gmail.com>
2022-02-13 00:49:17 +0000

    fixed git clone command

--- dependency.sh
+++ dependency.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get -y install postfix
 apt-get -y install sendemail
-git clone git clone https://github.com/kimocoder/wifite2.git
+git clone https://github.com/kimocoder/wifite2.git
 cd wifite2
 python3 setup.py install
 make deps
